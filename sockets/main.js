8f8ea63004fff282258aec53c286619d7779abf4
sank2000 <santhoshvelr@yahoo.com>
2021-07-15 13:00:21 +0530

    orbs added in backend ✔

--- sockets/main.js
+++ sockets/main.js
@@ -1,3 +1,32 @@
 const io = require('../servers').io;
 
+const Orb = require('./classes/Orb');
+
+let orbs = [];
+let settings = {
+  defaultOrbs: 50,
+  defaultSpeed: 6,
+  defaultSize: 6,
+  // as player gets bigger, the zoom needs to go out
+  defaultZoom: 1.5,
+  worldWidth: 500,
+  worldHeight: 500,
+};
+
+initGame();
+
+io.sockets.on('connect', (socket) => {
+  // a player has connected
+  socket.emit('initReturn', {
+    orbs,
+  });
+});
+
+// Run at the beginning of a new game
+function initGame() {
+  for (let i = 0; i < settings.defaultOrbs; i++) {
+    orbs.push(new Orb(settings));
+  }
+}
+
 module.exports = io;
