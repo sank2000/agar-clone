c2c52fbf5a2ba09d175298cbf3d35e830fe193c2
sank2000 <santhoshvelr@yahoo.com>
2021-07-16 10:58:49 +0530

    added player class 🤷‍♀️

--- sockets/main.js
+++ sockets/main.js
@@ -1,8 +1,12 @@
 const io = require('../servers').io;
 
 const Orb = require('./classes/Orb');
+const Player = require('./classes/Player');
+const PlayerData = require('./classes/PlayerData');
+const PlayerConfig = require('./classes/PlayerConfig');
 
 let orbs = [];
+let players = [];
 let settings = {
   defaultOrbs: 50,
   defaultSpeed: 6,
@@ -16,10 +20,17 @@ let settings = {
 initGame();
 
 io.sockets.on('connect', (socket) => {
+  let playerConfig = new PlayerConfig(settings);
+  // make a playerData object
+  let playerData = new PlayerData(data.playerName, settings);
+  // make a master player object to hold both
+  player = new Player(socket.id, playerConfig, playerData);
+
   // a player has connected
   socket.emit('initReturn', {
     orbs,
   });
+  players.push(playerData);
 });
 
 // Run at the beginning of a new game
