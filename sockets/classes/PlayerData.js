04d0c58acd23164b98d9b711af9c56ca4fd5e80c
sank2000 <santhoshvelr@yahoo.com>
2021-07-16 12:34:35 +0530

    added collision code 😐

--- sockets/classes/PlayerData.js
+++ sockets/classes/PlayerData.js
@@ -1,11 +1,15 @@
+const uuidv4 = require('uuid/v4');
+
 class PlayerData {
   constructor(playerName, settings) {
+    this.uid = uuidv4();
     this.name = playerName;
     this.locX = Math.floor(settings.worldWidth * Math.random() + 100);
     this.locY = Math.floor(settings.worldHeight * Math.random() + 100);
     this.radius = settings.defaultSize;
     this.color = this.getRandomColor();
     this.score = 0;
+    this.orbsAbsorbed = 0;
   }
   getRandomColor() {
     const r = Math.floor(Math.random() * 200 + 50);
